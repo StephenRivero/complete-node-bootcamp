93a582fc13e22390c10453c1b8d56c48e46c3e54
StephenRivero <stephen.m.rivero@gmail.com>
2026-03-10 10:40:14 +0800

    feat(server): implement dynamic template rendering for overview and product pages
    Added replaceTemplate function for dynamic HTML generation, fixed server to use http.createServer instead of non-existent Http2ServerRequest, implemented dynamic routing with url parsing for query parameters, and updated templates to include proper links with product IDs.

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 // const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -9,22 +11,66 @@ const fs = require("fs");
 
 ///////////////////////////////
 // SERVER
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObj = JSON.parse(data);
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
-const server = Http2ServerRequest.createServer((req, res) => {
-  const pathName = req.url;
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/starter/templates/template-overview.html`,
+  "utf-8",
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/starter/templates/template-card.html`,
+  "utf-8",
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/starter/templates/template-product.html`,
+  "utf-8",
+);
+
+const data = fs.readFileSync(
+  `${__dirname}/starter/dev-data/data.json`,
+  "utf-8",
+);
+const dataObj = JSON.parse(data);
+// console.log("dataObj", dataObj);
+
+const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    // console.log("cardsHtml", cardsHtml);
+    const output = tempOverview.replace("{%PRODUCT_CARDS%", cardsHtml);
+
+    res.end(output);
 
     // Product page
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
+  } else if (pathname === "/product") {
+    // console.log(query);
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
 
